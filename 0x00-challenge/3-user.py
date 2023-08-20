0e082ae6d40b4681be88a6ef593319db42c70cc2
Peter karanja <peterkaranja60@gmail.com>
2023-08-20 21:34:48 +0300

    Update 3-user.py

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-User Model
+ User Model
 """
 import hashlib
 import uuid
@@ -35,12 +35,12 @@ class User():
         Password setter:
         - `None` if `pwd` is `None`
         - `None` if `pwd` is not a string
-        - Hash `pwd` in MD5 before assigning to `__password`
+        - Hash `pwd` in MD5 before assign to `__password`
         """
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self._password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -84,17 +84,20 @@ if __name__ == '__main__':
     if user_2.password is not None:
         print("User.password should be None if setter to an integer")
 
-    if not user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right password")
+    if user_1.is_valid_password(u_pwd):
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right password")
+        print("is_valid_password should return False if it's not the right \
+password")
 
     if user_1.is_valid_password(None):
-        print("is_valid_password should return False if compared with None")
+        print("is_valid_password should return False if compare with None")
 
     if user_1.is_valid_password(89):
-        print("is_valid_password should return False if compared with an integer")
+        print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if no password is set before")
+        print("is_valid_password should return False if no password set \
+before")

c239208c3ee99210ee407d2e80b7ec04089c9c1a
Peter karanja <peterkaranja60@gmail.com>
2023-10-16 02:46:55 +0300

    0x01-challenge

--- 0x01-challenge/status_server/api/v1/views/__init__.py
+++ 0x01-challenge/status_server/api/v1/views/__init__.py
@@ -2,9 +2,6 @@
 """ Views module
 """
 from flask import Blueprint
-
-# Create a Blueprint for the views with the prefix '/api/v1'
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Import views from the 'index' module to define your API routes
 from api.v1.views.index import *
